d699fa5b88693d937774c3e51c67e75228ae6c0d
Dean Michael Ancajas <dbancajas@gmail.com>
2014-12-13 01:54:19 -0700

    xx

---
+++ include/datatypes.h
@@ -0,0 +1,18 @@
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+#include <vector>
+
+using std::vector;
+
+#define CUTOFF  50//limit combinations based on value;
+#define BIT 150
+
+typedef vector<int>::size_type vint_sz;
+typedef vector<vector<int> >::size_type vvint_sz;
+typedef vector<int>::const_iterator vint_itr;
+typedef vector<vector<int> >::const_iterator vvint_itr;
+
+	
+
+#endif
